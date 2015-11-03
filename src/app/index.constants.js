fde5f3b6fe424bf5c6cd1540fe8e24b2d0c1673a
Nick Portokallidis <portokallidis@gmail.com>
2015-11-03 11:28:23 +0200

    migrate project to gulp workflow using google guidelines

---
+++ src/app/index.constants.js
@@ -0,0 +1,9 @@
+/* global malarkey:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('cesGulp')
+    .constant('malarkey', malarkey);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('cesGulp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
