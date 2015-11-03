d17a463e3fbef4713c0b8a91d8c60f1f8f09b7c1
Nick Portokallidis <portokallidis@gmail.com>
2015-11-03 15:21:58 +0200

    change deploy env files

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 echo "Deploy to Docker server..."
 cd dist
+cp ../.env .
+cp ../.static .
 git init
 git remote add deploy dokku@athina.med.duth.gr:carre-entry-system
 git add .
 git commit -am"deploy"
-git push -f deploy
+git push -f deploy master
