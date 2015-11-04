f45547a54e01e6e1e11285bc530085663b090603
Nick Portokallidis <portokallidis@gmail.com>
2015-11-04 10:35:29 +0200

    migrate authentication

--- src/app/index.config.dev.js
+++ src/app/index.config.dev.js
@@ -8,7 +8,7 @@
     .constant("CONFIG", {
       "API": "//devices.carre-project.eu/ws/",
       "CARRE_DEVICES": "http://devices.carre-project.eu/devices/accounts/",
-      "TEST_TOKEN": "0213be219dc1821eb2f7b0bbc7c8a6cbe3c3559b",
+      // "TEST_TOKEN": "0213be219dc1821eb2f7b0bbc7c8a6cbe3c3559b",
       'ENV': 'DEV'
     })
     .config(function($locationProvider, $logProvider) {
