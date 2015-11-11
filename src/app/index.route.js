870ef5ca4e6df682f44cafaf44acae3f4e9930c6
Nick Portokallidis <portokallidis@gmail.com>
2015-11-11 14:55:10 +0200

    minor style changes and refactor controller

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -6,9 +6,9 @@
     .config(routerConfig);
 
   /** @ngInject */
-  function routerConfig($stateProvider,$urlRouterProvider) {
-    
-        
+  function routerConfig($stateProvider, $urlRouterProvider) {
+
+
     $stateProvider
       .state('main', {
         abstract: true,
@@ -16,23 +16,23 @@
         controllerAs: 'main',
         templateUrl: 'app/main/main.html',
         resolve: {
-          'currentUser':function(Auth){
+          'currentUser': function(Auth) {
             return Auth.getUser();
           },
-          'citationsArray': function(CARRE){
-               return CARRE.instances('citation');
+          'citationsArray': function(CARRE) {
+            return CARRE.instances('citation');
           },
-          'risk_elementsArray': function(CARRE){
-               return CARRE.instances('risk_element');
+          'risk_elementsArray': function(CARRE) {
+            return CARRE.instances('risk_element');
           },
-          'risk_factorsArray': function(CARRE){
-               return CARRE.instances('risk_factor');
+          'risk_factorsArray': function(CARRE) {
+            return CARRE.instances('risk_factor');
           },
-          'risk_evidencesArray': function(CARRE){
-               return CARRE.instances('risk_evidence');
+          'risk_evidencesArray': function(CARRE) {
+            return CARRE.instances('risk_evidence');
           },
-          'observablesArray': function(CARRE){
-               return CARRE.instances('observable');
+          'observablesArray': function(CARRE) {
+            return CARRE.instances('observable');
           }
         }
       })
@@ -44,7 +44,45 @@
         controller: 'citationsController',
         controllerAs: 'citations',
         templateUrl: 'app/citations/list.html',
-        url: '/citations'
+        url: '/citations',
+        resolve: {
+          'citations': function(citationsArray) {
+            
+            //check and reload state if the data is not the correct type ----- dont know why is happening -- api fault
+            if (citationsArray.data.some(function(obj) {
+                return obj.type.indexOf('http://carre.kmi.open.ac.uk/ontology/risk.owl#citation') === -1;
+              })) {
+              console.log('TELL ALLAN about this! It confused and instead of citation returned this: ', citationsArray.data[0].type[0]);
+              $state.reload();
+            }
+            else {
+              return citationsArray.data.map(function(obj) {
+                //console.info('Citations:',citationsArray);
+                /* Citations template
+                has_author: Array[2]
+                has_citation_pubmed_identifier: Array[1]
+                has_citation_source_level: Array[2]
+                has_citation_source_type: Array[2]
+                has_reviewer: Array[2]
+                id: "http://carre.kmi.open.ac.uk/citations/23271790"
+                type: Array[1]
+                */
+
+                //make label like this
+                // val.substring(val.lastIndexOf('/')+1));
+                return {
+                  has_author: obj.has_author ? obj.has_author[0] : '',
+                  has_author_label: obj.has_author ? obj.has_author[0].substring(obj.has_author[0].lastIndexOf('/') + 1) : '',
+                  has_reviewer: obj.has_reviewer ? obj.has_reviewer.join(',') : '',
+                  id: obj.has_citation_pubmed_identifier ? obj.has_citation_pubmed_identifier[0] : obj.id,
+                  has_citation_source_type: obj.has_citation_source_type ? obj.has_citation_source_type[0] : '',
+                  has_citation_source_level: obj.has_citation_source_level ? obj.has_citation_source_level[0] : '',
+                };
+              });
+            }
+
+          }
+        }
       })
       .state('main.observables', {
         controller: 'observablesController',
