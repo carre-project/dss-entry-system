dc849c7071e0c513ca17c2c5d1b3cb8649f051c6
Nick Portokallidis <portokallidis@gmail.com>
2015-12-18 13:59:21 +0200

    fix ui-grid fonts

--- gulp/build.js
+++ gulp/build.js
@@ -75,7 +75,7 @@ gulp.task('fonts', function () {
   return gulp.src($.mainBowerFiles())
     .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
     .pipe($.flatten())
-    .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/styles/')));
 });
 
 gulp.task('other', function () {
