33cd8db4e8b94649c9aa639ad4536cde05db2ffc
Nick Portokallidis <portokallidis@gmail.com>
2016-01-22 22:29:05 +0200

    fix undefined currentUseR

--- src/app/index.config.dev.js
+++ src/app/index.config.dev.js
@@ -12,6 +12,7 @@
       "BIOPORTAL_API_KEY": "a15281a9-d87d-4c0f-b7aa-31debe0f6449",
       "PUBMED_API_URL": "//eutils.ncbi.nlm.nih.gov/entrez/eutils/",
       // "TEST_TOKEN": "0213be219dc1821eb2f7b0bbc7c8a6cbe3c3559b",
+      "currentUser":{'guest':true},
       'ENV': 'DEV'
     })
     .config(function($locationProvider, $compileProvider, $urlRouterProvider) {
--- src/app/index.config.prod.js
+++ src/app/index.config.prod.js
@@ -10,7 +10,8 @@
       "CARRE_DEVICES": "//devices.carre-project.eu/devices/accounts/",
       "BIOPORTAL_API_URL": "//data.bioontology.org/",
       "BIOPORTAL_API_KEY": "a15281a9-d87d-4c0f-b7aa-31debe0f6449",
-      "PUBMED_API_URL": "//eutils.ncbi.nlm.nih.gov/entrez/eutils/",
+      "PUBMED_API_URL": "//eutils.ncbi.nlm.nih.gov/entrez/eutils/",     
+      "currentUser":{'guest':true},
       'ENV': 'PROD'
     }).config(function($locationProvider, $compileProvider, $urlRouterProvider) {
       
