bfad9e941b6e5207834681a02dffca46d3caedb9
Nick Portokallidis <portokallidis@gmail.com>
2016-02-03 19:50:52 +0200

    fix router

--- src/app/index.config.dev.js
+++ src/app/index.config.dev.js
@@ -4,10 +4,10 @@
   angular
     .module('CarreEntrySystem')
     .constant("CONFIG", {
-      "CARRE_API_URL": "https://devices.carre-project.eu/ws/",
+      "CARRE_API_URL": "http://devices.carre-project.eu/ws/",
       "CARRE_DEFAULT_GRAPH": "<http://carre.kmi.open.ac.uk/public>",
       "CARRE_ARCHIVE_GRAPH": "<http://carre.kmi.open.ac.uk/riskdata>",
-      "CARRE_DEVICES": "https://devices.carre-project.eu/devices/accounts/",
+      "CARRE_DEVICES": "http://devices.carre-project.eu/devices/accounts/",
       "BIOPORTAL_API_URL": "https://data.bioontology.org/",
       "BIOPORTAL_API_KEY": "a15281a9-d87d-4c0f-b7aa-31debe0f6449",
       "PUBMED_API_URL": "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/",
