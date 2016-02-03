6deb41e6e1f572862558e424fe8727af4c9cdc2c
Nick Portokallidis <portokallidis@gmail.com>
2016-02-03 19:48:29 +0200

    fix router

--- src/app/index.config.dev.js
+++ src/app/index.config.dev.js
@@ -4,13 +4,13 @@
   angular
     .module('CarreEntrySystem')
     .constant("CONFIG", {
-      "CARRE_API_URL": "//devices.carre-project.eu/ws/",
+      "CARRE_API_URL": "https://devices.carre-project.eu/ws/",
       "CARRE_DEFAULT_GRAPH": "<http://carre.kmi.open.ac.uk/public>",
       "CARRE_ARCHIVE_GRAPH": "<http://carre.kmi.open.ac.uk/riskdata>",
-      "CARRE_DEVICES": "//devices.carre-project.eu/devices/accounts/",
-      "BIOPORTAL_API_URL": "//data.bioontology.org/",
+      "CARRE_DEVICES": "https://devices.carre-project.eu/devices/accounts/",
+      "BIOPORTAL_API_URL": "https://data.bioontology.org/",
       "BIOPORTAL_API_KEY": "a15281a9-d87d-4c0f-b7aa-31debe0f6449",
-      "PUBMED_API_URL": "//eutils.ncbi.nlm.nih.gov/entrez/eutils/",
+      "PUBMED_API_URL": "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/",
       // "TEST_TOKEN": "0213be219dc1821eb2f7b0bbc7c8a6cbe3c3559b",
       "currentUser":{'guest':true},
       'ENV': 'DEV'
--- src/app/index.config.prod.js
+++ src/app/index.config.prod.js
@@ -4,13 +4,13 @@
   angular
     .module('CarreEntrySystem')
     .constant("CONFIG", {
-      "CARRE_API_URL": "//devices.carre-project.eu/ws/",
+      "CARRE_API_URL": "https://devices.carre-project.eu/ws/",
       "CARRE_DEFAULT_GRAPH": "<http://carre.kmi.open.ac.uk/public>",
       "CARRE_ARCHIVE_GRAPH": "<http://carre.kmi.open.ac.uk/riskdata>",
-      "CARRE_DEVICES": "//devices.carre-project.eu/devices/accounts/",
-      "BIOPORTAL_API_URL": "//data.bioontology.org/",
+      "CARRE_DEVICES": "https://devices.carre-project.eu/devices/accounts/",
+      "BIOPORTAL_API_URL": "https://data.bioontology.org/",
       "BIOPORTAL_API_KEY": "a15281a9-d87d-4c0f-b7aa-31debe0f6449",
-      "PUBMED_API_URL": "//eutils.ncbi.nlm.nih.gov/entrez/eutils/",     
+      "PUBMED_API_URL": "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/",     
       "currentUser":{'guest':true},
       'ENV': 'PROD'
     }).config(function($locationProvider, $compileProvider, $urlRouterProvider) {
@@ -18,7 +18,7 @@
       //show error
       $urlRouterProvider.otherwise( function($injector) {
         var $state = $injector.get("$state");
-        $state.go('/404_error');
+        $state.go('404_error');
       });
       
       //Set url handler  
