bff901474eba333bcaa968ffbee86c65dd2a0c12
Nick Portokallidis <portokallidis@gmail.com>
2016-04-06 10:03:50 +0300

    add the d3 colors

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -30,8 +30,11 @@
     };
     
     //chart colors
-    CONFIG.COLORS=["#46BFBD", "#F7464A","#2E8B57", "#DB7093", "#FDB45C", "#949FB1", "#4D5360",'#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928'];
+    // var jnnnColors=["#e08526", "#a118fb", "#2f739c", "#2db315", "#fb64b0", "#27af8d", "#4663db", "#706f42", "#ba4645", "#b68bce", "#ad36bc", "#ac9c1f", "#b3948f", "#2b7d1a", "#d32528", "#ad4a80", "#8f6521", "#4ea8ad", "#87a64a", "#3d7860", "#77687f", "#ea7790", "#fe4df0", "#cb2964", "#9d5962", "#f57550", "#d78670", "#4858fb", "#7499ee", "#c37eef", "#6f60bc", "#9a3cdb", "#7ca66d", "#107d40", "#3e6cbb", "#6ba78d", "#f067d0", "#939f8e", "#4f7741", "#bd4525", "#c52d81", "#c3924d", "#22a3ee", "#54717e", "#657061", "#58771c", "#d02747", "#766f1e", "#c589af", "#7d43fb", "#a696ae", "#81a0ae", "#8b6543", "#0d797e", "#69ad18", "#4dae6c", "#5fad49", "#a14c9e", "#8ea51c", "#c79223", "#a79d4b", "#bd936f", "#836661", "#9f9e6e", "#e26af0", "#65a1ce", "#63699d", "#a75823", "#935b7f", "#dd854e", "#9297ce", "#914fbc", "#d47bcf", "#b54863", "#d0878f", "#f17671", "#a08def", "#7753db", "#bb319e", "#e179af", "#f9742a", "#a35844", "#855d9d"]
+    var myColors = ["#46BFBD", "#F7464A","#2E8B57", "#DB7093", "#FDB45C", "#949FB1", "#4D5360",'#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928'];
     // ["#46BFBD", "#F7464A","#2E8B57", "#DB7093", "#FDB45C", "#949FB1", "#4D5360"]
+    
+    CONFIG.COLORS=myColors.concat(d3.scale.category20().range()); //mix the 20 default d3 color palette so you have almost 40
     Chart.defaults.global.colours=CONFIG.COLORS.map(function(color){return { fillColor: color }; });
     
     //toaster notification
